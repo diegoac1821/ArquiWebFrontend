1447ad4fe797ee1ffd4c14b5e6907ff59616fc09
diegoalhuay <diegoac1820@gmail.com>
2025-06-10 17:34:52 -0500

    listar usuario

--- AutoguardApp/src/app/services/usuario.service.ts
+++ AutoguardApp/src/app/services/usuario.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Usuario } from '../models/usuario';
+const base_url = environment.base;
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
 
-  constructor() { }
+  private url = `${base_url}/usuarios`;
+
+  constructor(private http: HttpClient) {}
+
+  list() {
+    return this.http.get<Usuario[]>(this.url);
+  }
 }
